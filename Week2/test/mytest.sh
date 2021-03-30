acd64e66f23e2965983c620ed2c4b49579e46ac9
Dang Khac Dat <khacdatdang@gmail.com>
2021-03-30 23:47:33 +0700

    Edit test file

--- Week2/test/mytest.sh
+++ Week2/test/mytest.sh
@@ -5,3 +5,5 @@
 ../incompleted/scanner ../test/example4.kpl | diff ../test/result4.txt -
 ../incompleted/scanner ../test/example5.kpl | diff ../test/result5.txt -
 ../incompleted/scanner ../test/example6.kpl | diff ../test/result6.txt -
+../incompleted/scanner ../test/example7.kpl | diff ../test/result8.txt -
+../incompleted/scanner ../test/example8.kpl | diff ../test/result8.txt -
